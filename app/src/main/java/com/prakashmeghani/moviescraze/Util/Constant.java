e1dab3633f1eb38d27be1569a50641ff172b229c
prakash meghani <prakashmeghan@gmail.com>
2017-12-20 12:11:44 +0530

    API_KEY constant
    API_KEY = "<place_your_api_key_here>"

--- app/src/main/java/com/prakashmeghani/moviescraze/Util/Constant.java
+++ app/src/main/java/com/prakashmeghani/moviescraze/Util/Constant.java
@@ -5,6 +5,7 @@ package com.prakashmeghani.moviescraze.Util;
  */
 
 public class Constant {
+    public static final String API_KEY = "<place_your_api_key_here>";
     public static final String QUERY_PARAM_API_KEY = "api_key";
     private static final String BASE_URL = "http://api.themoviedb.org/3/movie/";
     public static final String URL_POPULAR = BASE_URL + "popular";
